cd1bb65fca20f9bc9fba8df0a2b0fdf6e90fcec9
NissanthNadarassin <velanadarassin@gmail.com>
2024-12-04 17:43:09 +0100

    admin prev

--- home_decoration_page/WebServer/app/config/db.js
+++ home_decoration_page/WebServer/app/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost", 
-    USER: "s", 
-    PASSWORD: "",
+    USER: "Admin", 
+    PASSWORD: "Admin",
     DB: "home_decoration",
     dialect: "mysql",
     port: 3306,
